4949c43b88e0f945b35fc27dfea00812fb5b8e9e
Alfredo Urdaneta <alfredourdaneta@Alfredos-MacBook-Pro.local>
2024-06-06 17:56:03 -0400

    Fixed db name

--- src/database/index.ts
+++ src/database/index.ts
@@ -12,7 +12,7 @@ export const connectToDatabase = async () => {
   cached.promise =
     cached.promise ||
     mongoose.connect(MONGODB_URI, {
-      dbName: 'DashFusion',
+      dbName: 'aurda',
       bufferCommands: false,
     });
 
